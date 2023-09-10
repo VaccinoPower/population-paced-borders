ac91d4c09f7d81fb6f59ce6ac2585185543e7fc1
VaccinoPower <durdom2228@gmail.com>
2023-09-11 02:48:36 +0700

    Added plugin configuration, border expansion using custom formulas, and world-specific border adjustments.

--- src/main/java/com/example/PopulationPacedBordersPlugin.java
+++ src/main/java/com/example/PopulationPacedBordersPlugin.java
@@ -1,40 +1,127 @@
 package com.example;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.server.ServerListPingEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.World;
 import org.bukkit.WorldBorder;
 
 public class PopulationPacedBordersPlugin extends JavaPlugin implements Listener {
-    private final int CHUNK_SIZE = 16;
-    private final String WORLD_NAME = "world";
+    private int chunkSize;
+    String defaultFormula;
+    private ConfigurationSection worldsSection;
+    private int maxOnline;
+    private double borderRadius;
 
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
+        getConfig().options().copyDefaults(true);
+        saveConfig();
         loadConfig();
+        if (!validate()) {
+            return;
+        }
+        for (String worldName : worldsSection.getKeys(false)) {
+            updateWorldSize(maxOnline, worldName);
+        }
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
+        loadConfig();
         final int online = getServer().getOnlinePlayers().size();
-        try {
-            final WorldBorder worldBorder = getServer().getWorld(WORLD_NAME).getWorldBorder();
-            final double worldBorderSize = worldBorder.getSize();
-            if (worldBorderSize < online * CHUNK_SIZE) {
-                worldBorder.setSize(online * CHUNK_SIZE);
-            }
-        } catch (Exception npe) {
+        if (!validate() || online <= maxOnline) {
+            updateMaxOnline(online);
+            return;
+        }
+        updateMaxOnline(online);
+        for (String worldName : worldsSection.getKeys(false)) {
+            updateWorldSize(online, worldName);
+        }
+    }
+    @EventHandler
+    public void onServerListPing(ServerListPingEvent event) {
+        int chunkCount = (int)(borderRadius * borderRadius) * 4;
+        Component worldBorderInfo = Component.text()
+                .content("More players = bigger world. World border: ")
+                .append(Component.text(chunkCount, NamedTextColor.GOLD)
+                        .decoration(TextDecoration.BOLD, true))
+                .append(Component.text(" chunks.", NamedTextColor.GRAY))
+                .build();
+        event.motd(worldBorderInfo);
+    }
+
+    private boolean validate() {
+        if (chunkSize <= 0) {
+            getLogger().severe("chunk size must be positive. chunk_size is: \"" + chunkSize + "\". " +
+                    "World border remained unchanged.");
+            return false;
+        }
+        if (worldsSection == null) {
+            getLogger().severe("No worlds to change border" +
+                    "World border remained unchanged.");
+            return false;
+        }
+        return true;
+    }
+
+    private void updateWorldSize(int online, String worldName) {
+        World world = getServer().getWorld(worldName);
+        if (world == null) {
             getLogger().severe("An error occurred while working with WorldBorder. " +
-                    "Try changing the world name to \"world\": " + npe.getMessage());
-            npe.printStackTrace();
+                    "World name \"" + worldName + "\"" + "doesn't exist.");
+            return;
+        }
+        WorldBorder worldBorder = world.getWorldBorder();
+        String formula = worldsSection.getString(worldName + ".barrier_formula", defaultFormula);
+        double calculatedSize;
+        try {
+            calculatedSize = evaluateExpression(formula, online) * chunkSize;
+        } catch (Exception exception) {
+            getLogger().severe("There was an error in the formula:\"" + formula + "\". " +
+                    "World border remained unchanged.\n" + exception.getMessage());
+            return;
+        }
+        final double EPSILON = 1e-6;
+        if (Math.abs(worldBorder.getSize() - calculatedSize) > EPSILON) {
+            worldBorder.setSize(calculatedSize);
+            if (worldName.equals("world")) {
+                borderRadius = calculatedSize;
+            }
         }
     }
 
     private void loadConfig() {
-        getConfig().options().copyDefaults(true);
-        saveConfig();
+        FileConfiguration config = getConfig();
+        maxOnline = config.getInt("max_online", 1);
+        chunkSize = config.getInt("chunk_size", 16);
+        worldsSection = config.getConfigurationSection("worlds");
+        defaultFormula = config.getString("default_formula", "x");
     }
 
+    private void updateMaxOnline(int online) {
+        if (online > maxOnline) {
+            maxOnline = online;
+            getConfig().set("max_online", maxOnline);
+            saveConfig();
+        }
+    }
+
+    private double evaluateExpression(String expression, double x) {
+        Expression exp = new ExpressionBuilder(expression)
+                .variables("x")
+                .build()
+                .setVariable("x", x);
+        return exp.evaluate();
+    }
 }
--- src/test/java/com/example/PopulationPacedBordersPluginTest.java
+++ src/test/java/com/example/PopulationPacedBordersPluginTest.java
@@ -3,26 +3,40 @@ package com.example;
 import be.seeseemelk.mockbukkit.MockBukkit;
 import be.seeseemelk.mockbukkit.ServerMock;
 import be.seeseemelk.mockbukkit.WorldMock;
-import org.bukkit.WorldBorder;
-import org.junit.jupiter.api.*;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
 
 public class PopulationPacedBordersPluginTest {
-    private final int CHUNK_SIZE = 16;
-    private final int INITIAL_BORDER_SIZE = CHUNK_SIZE;
-    private final String WORLD_NAME = "world";
+    private static final int CHUNK_SIZE = 16;
+    private static final int MAX_PLAYERS = 64;
+    private static final int INITIAL_BORDER_SIZE = CHUNK_SIZE;
+    private final String[] WORLD_NAMES  = {"world", "world_nether", "world_the_end"};
     private ServerMock server;
-    private WorldBorder worldBorder;
-    private PopulationPacedBordersPlugin plugin;
+    private JavaPlugin plugin;
 
     @BeforeEach
     public void setUp() {
         server = MockBukkit.mock();
         plugin = MockBukkit.load(PopulationPacedBordersPlugin.class);
-        WorldMock world = new WorldMock();
-        world.setName(WORLD_NAME);
-        server.addWorld(world);
-        worldBorder = world.getWorldBorder();
-        worldBorder.setSize(INITIAL_BORDER_SIZE);
+        for (String worldName : WORLD_NAMES) {
+            plugin.getConfig().createSection("worlds." + worldName);
+            plugin.getConfig().set("chunk_size", CHUNK_SIZE);
+            WorldMock world = new WorldMock();
+            world.setName(worldName);
+            server.addWorld(world);
+            world.getWorldBorder().setSize(INITIAL_BORDER_SIZE);
+        }
+        plugin.saveConfig();
     }
 
     @AfterEach
@@ -30,38 +44,167 @@ public class PopulationPacedBordersPluginTest {
         MockBukkit.unmock();
     }
 
-    @Test
-    @DisplayName("Test border expansion with one player")
-    public void testBorderExpansionWithOnePlayer() {
-        server.setPlayers(1);
-        Assertions.assertEquals(CHUNK_SIZE, worldBorder.getSize(),
-                "World border is not expanded correctly.");
-    }
-
-    @Test
-    @DisplayName("Test border expansion with increasing players")
-    public void testBorderExpansionWithIncreasingPlayers() {
-        for (int numberOfPlayers = 2; numberOfPlayers < 64; numberOfPlayers++) {
-            server.setPlayers(numberOfPlayers);
-            Assertions.assertEquals(numberOfPlayers * CHUNK_SIZE, worldBorder.getSize(),
+    @ParameterizedTest
+    @MethodSource("playerCountProvider")
+    @DisplayName("Test border expansion with different player counts")
+    public void testBorderExpansionWithDifferentPlayerCounts(int playerCount, double expectedSize) {
+        server.setPlayers(playerCount);
+        for (String world : WORLD_NAMES) {
+            Assertions.assertEquals(expectedSize, server.getWorld(world).getWorldBorder().getSize(),
                     "World border is not expanded correctly.");
         }
+    }
+
+    private static Stream<Arguments> playerCountProvider() {
+        return Stream.of(
+                Arguments.of(1, CHUNK_SIZE),
+                Arguments.of(MAX_PLAYERS / 2, MAX_PLAYERS / 2 * CHUNK_SIZE),
+                Arguments.of(MAX_PLAYERS, MAX_PLAYERS * CHUNK_SIZE),
+                Arguments.of(0, INITIAL_BORDER_SIZE)
+        );
     }
 
     @Test
     @DisplayName("Test border expansion with decreasing players")
     public void testBorderExpansionWithDecreasingPlayers() {
-        server.setPlayers(64);
-        server.setPlayers(63);
-        Assertions.assertEquals(64 * CHUNK_SIZE, worldBorder.getSize(),
-                "World border is not expanded correctly.");
+        server.setPlayers(MAX_PLAYERS);
+        server.setPlayers(MAX_PLAYERS-1);
+        for (String world : WORLD_NAMES) {
+            Assertions.assertEquals(MAX_PLAYERS * CHUNK_SIZE, server.getWorld(world).getWorldBorder().getSize(),
+                    "World border is not expanded correctly.");
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("formulaProvider")
+    @DisplayName("Test barrier formula")
+    public void testBarrierFormula(String formula, int players, double expectedSize) {
+        for (String world : WORLD_NAMES) {
+            plugin.getConfig().set("worlds." + world + ".barrier_formula", formula);
+        }
+        server.setPlayers(players); // Установите желаемое количество игроков
+        for (String world : WORLD_NAMES) {
+            Assertions.assertEquals(expectedSize * CHUNK_SIZE, server.getWorld(world).getWorldBorder().getSize(),
+                    "World border is not expanded correctly.");
+        }
+    }
+
+    private static Stream<Arguments> formulaProvider() {
+        final int PLAYERS = MAX_PLAYERS / 16;
+        return Stream.of(
+                Arguments.of("1", PLAYERS, 1),
+                Arguments.of("1/3 + 1/3 + 1/3", PLAYERS, 1),
+                Arguments.of("1.25 * x", PLAYERS, 1.25 * PLAYERS),
+                Arguments.of("x * 2", PLAYERS, PLAYERS * 2),
+                Arguments.of("x + 5", PLAYERS, PLAYERS + 5),
+                Arguments.of("x - 1", PLAYERS, PLAYERS - 1),
+                Arguments.of("x / 2", PLAYERS, PLAYERS / 2),
+                Arguments.of("x ^ 2", PLAYERS, Math.pow(PLAYERS, 2)),
+                Arguments.of("sqrt(x)", PLAYERS, Math.sqrt(PLAYERS)),
+                Arguments.of("x * sin(x) * sin(x)", PLAYERS, PLAYERS * Math.sin(PLAYERS) * Math.sin(PLAYERS)),
+                Arguments.of("x * cos(x) * cos(x)", PLAYERS, PLAYERS * Math.cos(PLAYERS) * Math.cos(PLAYERS)),
+                Arguments.of("tan(x)", PLAYERS, Math.tan(PLAYERS)),
+                Arguments.of("log(x)", PLAYERS, Math.log(PLAYERS)),
+                Arguments.of("log10(x)", PLAYERS, Math.log10(PLAYERS)),
+                Arguments.of("exp(x)", PLAYERS, Math.exp(PLAYERS)),
+                Arguments.of("abs(x)", PLAYERS, Math.abs(-PLAYERS)),
+                Arguments.of("10 * x * abs(sin(x) + cos(x))", PLAYERS, 10 * PLAYERS * Math.abs(Math.sin(PLAYERS) + Math.cos(PLAYERS)))
+        );
     }
 
     @Test
-    @DisplayName("Test that the border size remains unchanged with no players")
-    public void testBorderSizeRemainsUnchangedWithNoPlayers() {
-        server.setPlayers(0);
-        Assertions.assertEquals(INITIAL_BORDER_SIZE, worldBorder.getSize(),
-                "The border size should not change without players.");
+    @DisplayName("Test barrier formula for different worlds")
+    public void testBarrierFormulaForDifferentWorlds() {
+        final int PLAYERS = MAX_PLAYERS / 16;
+
+        // Наборы формул для каждого мира
+        Map<String, String> formulas = new HashMap<>();
+
+        formulas.put("world", "x * 2");
+        formulas.put("world_nether", "x + 5");
+        formulas.put("world_the_end", "x / 4");
+
+        Map<String, Double> expectedSizes = new HashMap<>();
+        expectedSizes.put("world", PLAYERS * 2.0);
+        expectedSizes.put("world_nether", PLAYERS + 5.0);
+        expectedSizes.put("world_the_end", PLAYERS / 4.0);
+
+        for (Map.Entry<String, String> entry : formulas.entrySet()) {
+            String world = entry.getKey();
+            String formula = entry.getValue();
+            plugin.getConfig().set("worlds." + world + ".barrier_formula", formula);
+        }
+        server.setPlayers(PLAYERS);
+        for (Map.Entry<String, Double> entry : expectedSizes.entrySet()) {
+            String world = entry.getKey();
+            double expectedSize = entry.getValue();
+            Assertions.assertEquals(expectedSize * CHUNK_SIZE, server.getWorld(world).getWorldBorder().getSize(),
+                    "World border is not expanded correctly.");
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("chunkSizeProvider")
+    @DisplayName("Test border expansion with different chunk size")
+    public void testChunkSize(int chunkSize, String formula, int players, double expectedSize) {
+        plugin.getConfig().set("chunk_size", chunkSize);
+        for (String world : WORLD_NAMES) {
+            plugin.getConfig().set("worlds." + world + ".barrier_formula", formula);
+        }
+        server.setPlayers(players);
+        for (String world : WORLD_NAMES) {
+            Assertions.assertEquals(expectedSize * chunkSize, server.getWorld(world).getWorldBorder().getSize(),
+                    "World border is not expanded correctly.");
+        }
+    }
+
+    private static Stream<Arguments> chunkSizeProvider() {
+        String formula = "2 ^ x";
+        final int PLAYERS = MAX_PLAYERS / 16;
+        return Stream.of(
+                Arguments.of(1, formula, PLAYERS, Math.pow(2, PLAYERS)),
+                Arguments.of(8, formula, PLAYERS, Math.pow(2, PLAYERS)),
+                Arguments.of(16, formula, PLAYERS, Math.pow(2, PLAYERS))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidFormulaProvider")
+    public void testInvalidFormula(String invalidFormula) {
+        for (String world : WORLD_NAMES) {
+            plugin.getConfig().set("worlds." + world + ".barrier_formula", invalidFormula);
+        }
+        server.setPlayers(MAX_PLAYERS);
+        for (String world : WORLD_NAMES) {
+            Assertions.assertEquals(INITIAL_BORDER_SIZE, server.getWorld(world).getWorldBorder().getSize(),
+                    "World border is not expanded correctly.");
+        }
+    }
+
+    private static Stream<Arguments> invalidFormulaProvider() {
+        return Stream.of(
+                Arguments.of("x * 2 +"),
+                Arguments.of(""),
+                Arguments.of(")("),
+                Arguments.of("4)(4")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidChunkSizeProvider")
+    public void testInvalidChunkSize(int invalidChunkSize) {
+        plugin.getConfig().set("chunk_size", invalidChunkSize);
+        server.setPlayers(MAX_PLAYERS);
+        for (String world : WORLD_NAMES) {
+            Assertions.assertEquals(INITIAL_BORDER_SIZE, server.getWorld(world).getWorldBorder().getSize(),
+                    "World border is not expanded correctly.");
+        }
+    }
+
+    private static Stream<Arguments> invalidChunkSizeProvider() {
+        return Stream.of(
+                Arguments.of(-1),
+                Arguments.of(0)
+        );
     }
 }
